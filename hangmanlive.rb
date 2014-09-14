7faa03a99039e027541d03e711b054480f12164c
stephanie pi <stephmpi@gmail.com>
2014-09-13 23:04:53 -0700

    added wrong guesses to display, added colors, and fixed game so that it also ended when you win

--- hangmanlive.rb
+++ hangmanlive.rb
@@ -3,58 +3,37 @@ require "colorize"
 class Hangman
 
   def initialize
-    @head        = " "
-    @l_arm       = " "
-    @r_arm       = " "
-    @body        = " "
-    @l_leg       = " "
-    @r_leg       = " "
-    @secret      = random_word
-    @characters  = empty_characters
-    @guesses     = []
+    @head         = " "
+    @l_arm        = " "
+    @r_arm        = " "
+    @body         = " "
+    @l_leg        = " "
+    @r_leg        = " "
+    @secret       = random_word
+    @characters   = empty_characters
+    @guesses      = []
     @wrongguesses = []
   end
 
-#creates a new array of empty characters based on the length of the chosen word
+  #creates a new array of empty characters based on the length of the chosen word
   def empty_characters
     Array.new(@secret.length, "-")
-    # a = []
-    # @secret.length.times {a << "_"}
   end
 
+  #while this method evaluates to false, the while loop in .run will keep running.
   def game_win
     words.each do |answer|
       if guess == answer
-        puts "You win. The answer is: #{answer}!"
+        puts "You win.".light_green.blink
+        puts "The answer is: #{answer}!"
+        return true
       elsif @wrongguesses.length >= 6
-        puts "Sorry, you lost"
+        puts "Sorry, you lost. The answer was #{answer}"
         return true
       end
     end
     return false
   end
-
-  # def game_lose
-  #   if @wrongguesses.length >= 7
-  #     puts "Sorry, you lost"
-  #   end
-  # end
-
-  # def guessed(guessed_letter)
-  #   @guesses << guessed_letter
-  # end
-
-
-  def addguess(input)
-    @guesses << input
-  end
-
-  def oneguess(input)
-    if @guesses.include?(input)
-      puts "You already guessed that"
-    end
-  end
-
 
 
 #checks secret word to see if the letters match, then uses the index of the secret array and puts the letters on the characters array
@@ -66,12 +45,28 @@ class Hangman
         @characters[index] = letter
       end
     end
-    if !@secret.chars.include?(guessed_letter)
-      @wrongguesses << guessed_letter
+    if not_in_secret?(guessed_letter) && not_in_wrong?(guessed_letter)
+       @wrongguesses << (guessed_letter)
     end
   end
 
+  def not_in_secret?(letter)
+    !@secret.chars.include?(letter)
+  end
 
+  def not_in_wrong?(letter)
+    !@wrongguesses.include?(letter)
+  end
+
+  def addguess(input)
+    @guesses << input
+  end
+
+  def oneguess(input)
+    if @guesses.include?(input)
+      puts "You already guessed that"
+    end
+  end
 
 #prints the contents of the array
   def guess
@@ -86,8 +81,12 @@ class Hangman
     words.sample
   end
 
+  def display_wrongguesses
+    puts "You've already guessed these letters: #{@wrongguesses.join}"
+  end
+
   def words
-    ["justice", "latina", "feminism", "culture", "knowledge", "mujerista", "communist"]
+    ["justice", "latina", "feminism", "culture", "knowledge", "mujerista"]
   end
 
   def addbody
@@ -121,7 +120,6 @@ class Hangman
     end
   end
 
-
   def board
     <<-BOARD
 ***************************************************
@@ -134,19 +132,7 @@ _____|
 ________
      BOARD
   end
-
 end
-
-
-#Draw and print the board
-# secret word
-# ***take a guess and compare it to the secret word, update the characters with the guessed letter
-# make concept of blank spaces
-# remember guesses
-# remember which parts have been added
-# it should know when the game is won or lost
-# it should know if a guess is already been guessed
-# body parts need color
 
 def run
   h = Hangman.new
@@ -156,11 +142,9 @@ def run
       puts h.guess
       puts "What letter do you want to guess?\n"
       guess = gets.chomp.downcase
+      h.display_wrongguesses
       h.oneguess(guess)
       h.make_guess(guess)
-      h.addguess(guess)
-
     end
 end
-
 run
